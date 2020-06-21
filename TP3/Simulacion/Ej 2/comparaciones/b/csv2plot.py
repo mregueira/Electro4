76b7bb6f819c40593fce874308ee801e05dd824f
amartorell <amartorell@itba.edu.ar>
2020-06-20 23:10:40 -0300

    subo las imagenes de la fft

--- TP3/Simulacion/Ej 2/comparaciones/b/csv2plot.py
+++ TP3/Simulacion/Ej 2/comparaciones/b/csv2plot.py
@@ -11,42 +11,42 @@ y2 = []
 x3 = []
 y3 = []
 
-fileName="C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\b\\Ej2"
+fileName="C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\Ej2"
 saveFiles=True
 
 from distutils.spawn import find_executable
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\b\\2bvi08.csv', 'r') as csvfile: #aca cargo los datos de una curva en x e y
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cfft15.csv', 'r') as csvfile: #aca cargo los datos de una curva en x e y
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x1.append(float(row[0]))
-        y1.append(float(row[2]))
+        y1.append(10 ** (float(row[1])/20))
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\b\\2bvi1.csv', 'r') as csvfile:  # aca empieza otra curva
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cfft21.csv', 'r') as csvfile:  # aca empieza otra curva
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x2.append(float(row[0]))
-        y2.append(float(row[2]))
+        y2.append(10 ** (float(row[1])/20))
 
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\b\\2bvi13.csv', 'r') as csvfile:#aca empieza otra curva
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cfft27.csv', 'r') as csvfile:#aca empieza otra curva
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x3.append(float(row[0]))
-        y3.append(float(row[2]))
+        y3.append(10 ** (float(row[1])/20))
 
 #grafico 1
 
 fig, graph1 = plt.subplots() #todo lo escrito en el grafico se configura aca
 #graph1.set_ylim([-100,20])
-graph1.set_xlim([0,0.025])
+graph1.set_xlim([20,1e4])
 graph1.set_xlabel('t')
 graph1.set_ylabel('I')
-graph1.set_title('Corriente con ma=0.8')
+graph1.set_title('Espectro de la corriente con ma=0.8')
 graph1.grid()
 #graph1.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
-#graph1.set_xscale('log')
-graph1.plot(x1,y1,color='cyan', linewidth=1.3, label='ma=0.8', linestyle='dashed')  # plot vgs
+graph1.set_xscale('log')
+graph1.plot(x1,y1,color='goldenrod', linewidth=1.3, label='mf=15', linestyle='dashed')  # plot vgs
 #graph1.tick_params(axis='y', labelcolor='r')#esto es para el color de los numeritos
 graph1.legend(loc=('upper left'))
 
@@ -55,21 +55,27 @@ graph1.legend(loc=('upper left'))
 graph2 = graph1#.twinx() #esto es para hacer una escala diferente para cada plot
 #graph2.set_ylim([-2.5,1])
 #graph2.set_ylabel('dB')
+graph2.set_xlim([20,1e4])
+graph2.set_xscale('log')
+
 #graph2.tick_params(axis='y', labelcolor='b')
-graph2.plot(x2,y2,label='ma=1', color='magenta', linewidth=1.3, linestyle='dashed')
+graph2.plot(x2,y2,label='mf=21', color='lawngreen', linewidth=1.3, linestyle='solid')
 graph2.legend(loc='upper left')
 
 
 #grafico 3
 graph3 = graph2
+graph3.set_xlim([20,1e4])
+graph3.set_xscale('log')
+
 #graph3.set_ylim([-2.5,1])
 #graph3.set_ylabel('dB')
 #graph3.tick_params(axis='y', labelcolor='g')
-graph3.plot(x3,y3,label='ma=1.3', color='darkorange', linewidth=1.3, linestyle='dashed')
+graph3.plot(x3,y3,label='mf=27', color='steelblue', linewidth=1.3, linestyle='dashed')
 graph3.legend(loc='upper left')
 
 
 
 if saveFiles:#si quiero guardar el archivo, lo guardo
-    plt.savefig(fileName+"b_I"+".png")
+    plt.savefig(fileName+"bfft"+".png")
 plt.show()
--- TP3/Simulacion/Ej 2/comparaciones/b/plot_different_plots.py
+++ TP3/Simulacion/Ej 2/comparaciones/b/plot_different_plots.py
@@ -11,22 +11,22 @@ y2 = []
 x3 = []
 y3 = []
 
-fileName="C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\Ej2"
+fileName="C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\d\\Ej2"
 saveFiles=True
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cvi15.csv', "r") as csvfile: #aca cargo los datos de una curva en x e y
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\d\\2dvi33.csv', "r") as csvfile: #aca cargo los datos de una curva en x e y
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x1.append(float(row[0]))
         y1.append(float(row[1]))
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cvi21.csv', 'r') as csvfile:#aca empieza otra curva
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\d\\2dvi66.csv', 'r') as csvfile:#aca empieza otra curva
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x2.append(float(row[0]))
         y2.append(float(row[1]))
 
-with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\c\\2cvi27.csv', 'r') as csvfile:  # aca empieza otra curva
+with open('C:\\Users\\marth\\Documents\\ITBA\\Eda\\Electro4\\TP3\\Simulacion\\Ej 2\\comparaciones\\d\\2dvi81.csv', 'r') as csvfile:  # aca empieza otra curva
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x3.append(float(row[0]))
@@ -64,27 +64,27 @@ fig, axs = plt.subplots(3, 1, sharex=True)
 fig.subplots_adjust(hspace=0.1)
 
 # Plot each graph, and manually set the y tick values
-axs[0].plot(x1, y1, label='ma=0.8', color='cyan', linewidth=1.3)
+axs[0].plot(x1, y1, label='mf=33', color='cyan', linewidth=1.3)
 #axs[0].set_title('Tensión para diferentes valores de R')
-axs[0].set_title('Comparación de tensiones con mf=15')
+axs[0].set_title('Comparación de tensiones con ma=1.1')
 axs[0].set_xlim([0,0.025])
 axs[0].legend()
 #axs[0].set_yticks(np.arange(-0.9, 1.0, 0.4))
 #axs[0].set_ylim(-1, 1)
 
-axs[1].plot(x2, y2, label='ma=1', color='magenta', linewidth=1.3)
+axs[1].plot(x2, y2, label='mf=66', color='magenta', linewidth=1.3)
 axs[1].set_ylabel('V')
 axs[1].legend()
 #axs[1].set_yticks(np.arange(0.1, 1.0, 0.2))
 #axs[1].set_ylim(0, 1)
 
-axs[2].plot(x3, y3, label='ma=1.3', color='darkorange', linewidth=1.3)
+axs[2].plot(x3, y3, label='mf=81', color='darkorange', linewidth=1.3)
 axs[2].set_xlabel('t(s)')
 axs[2].legend()
 #axs[2].set_yticks(np.arange(-0.9, 1.0, 0.4))
 #axs[2].set_ylim(-1, 1)
 
 
-plt.savefig(fileName+"b_V"+".png")
+plt.savefig(fileName+"d_V"+".png")
 
 plt.show()
